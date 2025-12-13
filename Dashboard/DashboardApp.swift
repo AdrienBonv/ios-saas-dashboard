37d35639e8ce50f9348f62c5f7f609213b0b828c
AdrienBonv <60156774+AdrienBonv@users.noreply.github.com>
2025-12-13 10:59:01 +0100

    Initial Commit

---
+++ Dashboard/DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardApp.swift
+//  Dashboard
+//
+//  Created by Adrien BONVALLET on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
